40fc67495cbbb62cd303d55669354758570772b4
kklave <kklave@yahoo.com>
2025-04-03 17:20:53 -0700

    Final app - step 10

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -14,14 +14,18 @@ function MyApp() {
     setCharacters(updated);
   }
 
+  function updateList(person) {
+    setCharacters([...characters, person]);
+  }
+
   return (
     <div className="container">
       <Table
         characterData={characters}
         removeCharacter={removeOneCharacter}
       />
-      <Form />
-    </div>
+      <Form handleSubmit={updateList} />    
+      </div>
   );
 }
 
