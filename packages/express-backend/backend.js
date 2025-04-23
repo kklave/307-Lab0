ebe551fb8ade9e2795e81b131c871610c1f89721
kklave <kklave@yahoo.com>
2025-04-22 19:32:46 -0700

    working with database, cleaned up commented out code

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -8,65 +8,6 @@ import x from "./user-services.js"
 const app = express();
 const port = 8000;
 
-// const users = {
-//   users_list: [
-//     {
-//       id: "xyz789",
-//       name: "Charlie",
-//       job: "Janitor"
-//     },
-//     {
-//       id: "abc123",
-//       name: "Mac",
-//       job: "Bouncer"
-//     },
-//     {
-//       id: "ppp222",
-//       name: "Mac",
-//       job: "Professor"
-//     },
-//     {
-//       id: "yat999",
-//       name: "Dee",
-//       job: "Aspring actress"
-//     },
-//     {
-//       id: "zap555",
-//       name: "Dennis",
-//       job: "Bartender"
-//     }
-//   ]
-// };
-
-const generateID = (user) => {
-  user.id = (Math.random() * 100).toString();
-}
-
-const findUserByName = (name) => {
-  return users["users_list"].filter(
-    (user) => user["name"] === name
-  );
-};
-
-const findUserByJob = (job) => {
-  return users["users_list"].filter(
-    (user) => user["job"] === job
-  );
-};
-
-const findUserById = (id) => {
-  return users["users_list"].find((user) => user["id"] === id);
-}
-
-const addUser = (user) => {
-  users["users_list"].push(user);
-  return user;
-};
-
-const removeUser = (id) => {
-  users.users_list = users.users_list.filter((user) => user.id !== id);
-};
-
 app.use(cors());
 app.use(express.json());
 
@@ -89,25 +30,6 @@ app.get("/users", (req, res) => {
     });
 });
 
-// app.get("/users", (req, res) => {
-//   const name = req.query.name;
-//   const job = req.query.job;
-//   let result;
-//   if ((name != undefined) && (job != undefined))
-//   {
-//     result = findUserByName(name);
-//     result = findUserByJob(job);
-//     result = { users_list: result };
-//     res.send(result);
-//   }
-//   else if (name != undefined) {
-//     result = findUserByName(name);
-//     result = { users_list: result };
-//     res.send(result);
-//   } else {
-//     res.send(users);
-//   }
-// });
 
 app.get("/users/:id", (req, res) => {
   const id = req.params["id"]; //or req.params.id
@@ -126,15 +48,6 @@ app.get("/users/:id", (req, res) => {
     })
 });
 
-// app.get("/users/:id", (req, res) => {
-//   const id = req.params["id"]; //or req.params.id
-//   let result = findUserById(id);
-//   if (result === undefined) {
-//     res.status(404).send("Resource not found.");
-//   } else {
-//     res.send(result);
-//   }
-// });
 
 app.post("/users", (req, res) => {
   const userToAdd = req.body;
@@ -148,12 +61,6 @@ app.post("/users", (req, res) => {
     })
 });
 
-// app.post("/users", (req, res) => {
-//   const userToAdd = req.body;
-//   generateID(userToAdd);
-//   addUser(userToAdd);
-//   res.status(201).json(userToAdd);
-// });
 
 app.delete("/users/:id", (req, res) => {
   const id = req.params.id; //or req.params.id
@@ -162,7 +69,7 @@ app.delete("/users/:id", (req, res) => {
     .then((user) => {
       if (user) {
         return user.deleteOne().then(() => {
-          res.status(204).send("Deleted");
+          res.status(204).send();
         });
       }
       else {
@@ -174,19 +81,6 @@ app.delete("/users/:id", (req, res) => {
     })
 });
 
-// app.delete("/users/:id", (req, res) => {
-//   const id = req.params.id; //or req.params.id
-//   const userToRemove = findUserById(id);
-//   if (userToRemove === undefined)
-//   {
-//     res.status(404).send("Resource not found.");
-//   }
-//   else 
-//   {
-//     removeUser(id);
-//     res.status(204).send();
-//   }
-// });
 
 app.listen(port, () => {
   console.log(
